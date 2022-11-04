ef30b829388291dce6505c77fe45db79e85b3472
alaskaThunderfx <smackey85@yahoo.com>
2022-11-03 22:49:25 -0400

    Begin work on Fireball weapon
    Added the basic model and particles for the Fireball, needed a break
    from the enemies, as there does, in fact, seem to be some syncing issues
    and I'm going to have to re-evaluate how I approach it.
    
    Anyway, well begin further work on the firball tomorrow.

---
+++ Assets/_scripts/Player/Fireball.cs
@@ -0,0 +1,195 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Fireball : UdonSharpBehaviour
+{
+    [Header("Components")]
+    public VRCPlayerApi Owner;
+    public PlayerController PlayerController;
+    public EffectsContainer EffectsContainer;
+    public FireballPool FireballPool;
+    public Rigidbody FireballRB;
+
+    [Header("Fireball Info")]
+    // Used to initialize where the Fireball will spawn in reference to the current player
+    public Vector3 FireballPosition;
+    public Quaternion FireballRotation;
+
+    // To store Enemy hit info
+    public Collider Enemy;
+
+    // For tracking distance
+    public float DistanceTravelled;
+
+    [Header("Sounds and VFX")]
+    // Used to detect if what was collided with was an enemy
+    public bool HitEnemy;
+
+    // Checks to see if what was hit was an inanimate object
+    public bool HitIAO;
+
+    // The effect when the Fireball hits an IAO
+    public ParticleSystem Spark;
+
+    // The effect for when a Fireball hits an enemy
+    public ParticleSystem Blood;
+
+    // The sound made when the player throws the Fireball
+    private AudioClip Throw;
+
+    // The sound when a Fireball hits an IAO
+    private AudioClip HitIAOSound;
+
+    // The sound when a Fireball hits an enemy
+    private AudioClip HitEnemySound;
+
+    // The sound made by the enemy when killed
+    private AudioClip Kill;
+
+    // The position to player the effects at
+    public Vector3 EffectPosition;
+
+    // for OnEnable functionality
+    public bool ReadyToGo = false;
+    public Transform mesh;
+    public Transform particle;
+    public Vector3 OgMesh;
+    public Vector3 OgParticle;
+    public bool HasStarted;
+
+    // Ran after the owner has been set on this object
+    public void _OnOwnerSet()
+    {
+        Debug.Log("In _OnOwnerSet in Fireball");
+        // Set AudioClips
+        Owner = Networking.GetOwner(gameObject);
+        FireballPool = transform.parent.GetComponent<FireballPool>();
+
+        PlayerController = FireballPool.PlayerController;
+        EffectsContainer = FireballPool.EffectsContainer;
+        Spark = EffectsContainer.Spark;
+        Blood = EffectsContainer.Blood;
+        Throw = EffectsContainer.Throw.clip;
+        HitIAOSound = EffectsContainer.IAOHit.clip;
+        HitEnemySound = EffectsContainer.EnemyHit.clip;
+        Kill = EffectsContainer.Kill.clip;
+        gameObject.SetActive(false);
+    }
+
+    // When it's the Fireball's turn to be thrown
+    private void OnEnable()
+    {
+        if (ReadyToGo == false)
+        {
+            return;
+        }
+        FireballPool = transform.parent.GetComponent<FireballPool>();
+        FireballRB = transform.GetComponent<Rigidbody>();
+        DistanceTravelled = 0;
+        HitEnemy = false;
+        HitIAO = false;
+
+        FireballPosition = new Vector3(
+            Random.Range(FireballPool.PlayerPosition.x - .2f, FireballPool.PlayerPosition.x + .2f),
+            Random.Range(.8f, 1.2f),
+            FireballPool.PlayerPosition.z
+        );
+        FireballRotation = FireballPool.PlayerRotation;
+
+        transform.SetPositionAndRotation(FireballPosition, FireballRotation);
+        FireballRB.velocity = transform.forward * FireballPool.Force;
+
+        mesh = transform.GetChild(0);
+        particle = transform.GetChild(1);
+        OgMesh = mesh.localScale;
+        OgParticle = particle.localScale;
+        mesh.localScale = new Vector3(
+            OgMesh.x * FireballPool.Size,
+            OgMesh.y * FireballPool.Size,
+            OgMesh.z * FireballPool.Size
+        );
+        particle.localScale = new Vector3(
+            OgParticle.x * FireballPool.Size,
+            OgParticle.y * FireballPool.Size,
+            OgParticle.z * FireballPool.Size
+        );
+        AudioSource.PlayClipAtPoint(Throw, transform.position, 0.1f);
+    }
+
+    private void Update()
+    {
+        if (ReadyToGo == false)
+        {
+            return;
+        }
+        // Tracking Distance
+        DistanceTravelled += Vector3.Distance(transform.position, FireballPosition);
+        if (DistanceTravelled >= FireballPool.Range)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EffectPosition = transform.position;
+        if (other.name.Contains("IAE"))
+        {
+            Debug.Log("Hit an inanimate object");
+            HitIAO = true;
+            gameObject.SetActive(false);
+        }
+        else if (other.name.Contains("Enemy"))
+        {
+            Debug.Log(Owner + " hit a " + other.name + "!");
+            HitEnemy = true;
+            Enemy = other;
+            AudioSource.PlayClipAtPoint(HitEnemySound, transform.position);
+            Blood.transform.position = transform.position;
+            Blood.Play(true);
+            Networking.SetOwner(Owner, other.gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (ReadyToGo)
+        {
+            mesh.localScale = OgMesh;
+            particle.localScale = OgParticle;
+        }
+
+        if (HitIAO)
+        {
+            AudioSource.PlayClipAtPoint(HitIAOSound, transform.position, 0.1f);
+            Spark.transform.position = transform.position;
+            Spark.Play(true);
+        }
+
+        ReadyToGo = true;
+        if (HitEnemy && Networking.LocalPlayer == Owner)
+        {
+            if (Enemy.name.Contains("LilSnek"))
+            {
+                LilSnek LilSnek = Enemy.GetComponent<LilSnek>();
+                LilSnek.Health -= FireballPool.Damage;
+            }
+            else if (Enemy.name.Contains("LilWolf"))
+            {
+                LilWolf LilWolf = Enemy.GetComponent<LilWolf>();
+                LilWolf.Health -= FireballPool.Damage;
+            }
+            else if (Enemy.name.Contains("LilGhost"))
+            {
+                LilGhost LilGhost = Enemy.GetComponent<LilGhost>();
+                LilGhost.Health -= FireballPool.Damage;
+            }
+            HitEnemy = false;
+        }
+    }
+
+    public void EnemyInteraction(Collider Enemy) { }
+}
---
+++ Assets/_scripts/Player/FireballPool.cs
@@ -0,0 +1,333 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Udon.Common.Interfaces;
+using VRC.SDKBase;
+using VRC.SDK3.Components;
+using VRC.Udon;
+
+public class FireballPool : UdonSharpBehaviour
+{
+    [Header("Owner Information")]
+    public VRCPlayerApi Owner;
+
+    // Player tracking
+    public Vector3 PlayerPosition;
+    public Quaternion PlayerRotation;
+    public Transform StartSpot;
+
+    [Header("Prefab Components")]
+    // Main script attached to player
+    public PlayerController PlayerController;
+
+    // Script attached to the EffectsContainer
+    public EffectsContainer EffectsContainer;
+    public PlayerUI PlayerUI;
+    public Scoreboard Scoreboard;
+
+    [Header("Fireball Stats")]
+    // Fireball array
+    public GameObject[] Knives = new GameObject[30];
+
+    // Used to toggle the Fireball weapon
+    [UdonSynced]
+    public bool isFireballOn = false;
+
+    // Index used for iterating through the pool
+    public int FireballIndex;
+
+    [UdonSynced]
+    public int Level;
+
+    [UdonSynced, FieldChangeCallback(nameof(XP))]
+    public float xP;
+    public float XPToNextLv;
+
+    // [UdonSynced, FieldChangeCallback(nameof(HP))]
+    public float hP;
+
+    // HPlvl tracker
+    public int TrackHPLv;
+
+    [UdonSynced]
+    public float DEF;
+    public int TrackDEFLv;
+
+    [UdonSynced, FieldChangeCallback(nameof(RunSpeed))]
+    public float runSpeed;
+    public int TrackRSLv;
+
+    // Amount of damage done on hit
+    [UdonSynced]
+    public float Damage;
+    public int TrackDMGLv;
+
+    // Attack frequency
+    [UdonSynced]
+    public float Cooldown;
+    public int TrackCDLv;
+
+    // Distance knives get thrown
+    [UdonSynced]
+    public float Range;
+    public int TrackRangeLv;
+
+    // Amount of knives thrown at once
+    [UdonSynced]
+    public float Quantity;
+    public int TrackQTYLv;
+
+    [UdonSynced]
+    public float Size;
+    public int TrackSizeLv;
+
+    // Throw speed
+    [UdonSynced]
+    public float Force;
+    public int TrackForceLv;
+
+    // Counter for the cooldown
+    public float CDCounter;
+
+    // Upgrade tracker
+    public int[] Upgrades = new int[9];
+
+    // Correlating value for index position
+    // 0 - HP
+    // 1 - DEF
+    // 2 - RunSpeed
+    // 3 - AttackDMG
+    // 4 - Cooldown
+    // 5 - AttackSpeed
+    // 6 - Range
+    // 7 - Size
+    // 8 - Quantity
+
+    public bool ReadyToGo = false;
+    public bool SwitchOnUI = false;
+
+    private void OnEnable() {
+        _OnOwnerSet();
+        isFireballOn = true;
+    }
+
+    // Script ran after the _OnOwnerSet script on the PlayerController
+    public void _OnOwnerSet()
+    {
+        PlayerController = transform.parent.GetComponent<PlayerController>();
+        Owner = PlayerController.Owner;
+        PlayerUI = GameObject.Find("PlayerUI").GetComponent<PlayerUI>();
+        EffectsContainer = PlayerController.EffectsContainer;
+
+        // Reset stats to base
+        Level = 1;
+        XP = 0;
+        XPToNextLv = 3f;
+        HP = 10f;
+        TrackHPLv = 0;
+        DEF = 0f;
+        TrackDEFLv = 0;
+        RunSpeed = 5f;
+        TrackRSLv = 0;
+        Damage = 1f;
+        TrackDMGLv = 0;
+        Cooldown = 2f;
+        TrackCDLv = 0;
+        Range = 100f;
+        TrackRangeLv = 0;
+        Quantity = 1f;
+        TrackQTYLv = 0;
+        Size = 1f;
+        TrackSizeLv = 0;
+        Force = 2f;
+        TrackForceLv = 0;
+
+        int index = 0;
+        foreach (Transform child in transform)
+        {
+            Networking.SetOwner(Owner, child.gameObject);
+            Fireball Fireball = child.GetComponent<Fireball>();
+            Fireball.name = index.ToString() + "Fireball";
+            Knives[index] = Fireball.gameObject;
+            Fireball._OnOwnerSet();
+            index++;
+        }
+
+        for (int i = 0; i < Upgrades.Length; i++)
+        {
+            Upgrades[i] = 0;
+        }
+        ReadyToGo = true;
+    }
+
+    private void Update()
+    {
+        if (Owner == null || !ReadyToGo)
+        {
+            return;
+        }
+        // Tracking the players position
+        PlayerPosition = Owner.GetPosition();
+        PlayerRotation = Owner.GetRotation();
+
+        if (SwitchOnUI)
+        {
+            PlayerController.PlayerUI.AttackToggleOn.gameObject.SetActive(true);
+            SwitchOnUI = false;
+        }
+
+        // Used to toggle Fireball attacking
+        // If isFireballOn is false, stop attacking
+        if (!isFireballOn)
+        {
+            return;
+        }
+        // if isFireballOn is true, begin the attack
+        else
+        {
+            // Countdown from the CDCounter
+            CDCounter -= Time.deltaTime;
+            // Once the CDCounter is less than or equal to 0
+            if (CDCounter <= 0)
+            {
+                // Reset CDCounter to Cooldown value
+                CDCounter = Cooldown;
+                // Begin for loop that corresponds with Quantity value
+                // Trying a switch statement instead
+                switch (Quantity)
+                {
+                    case 1:
+                        ThrowFireball();
+                        break;
+                    case 2:
+                        ThrowFireball();
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.1f);
+                        break;
+                    case 3:
+                        ThrowFireball();
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.1f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.2f);
+                        break;
+                    case 4:
+                        ThrowFireball();
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.1f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.2f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.3f);
+                        break;
+                    case 5:
+                        ThrowFireball();
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.1f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.2f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.3f);
+                        SendCustomEventDelayedSeconds(nameof(ThrowFireball), 0.4f);
+                        break;
+                }
+            }
+        }
+    }
+
+    public void ThrowFireball()
+    {
+        // Set Fireball at this index in the object pool to active
+        Knives[FireballIndex].SetActive(true);
+        // If the value of Fireball index is less than or equal to 28
+        if (FireballIndex <= 28)
+        {
+            // Increase FireballIndex value
+            FireballIndex++;
+        }
+        // If the value is 29
+        else
+        {
+            // Reset value to zaro
+            FireballIndex = 0;
+        }
+    }
+
+    public float maxHP;
+    public float MaxHP
+    {
+        set
+        {
+            maxHP = HP + value;
+            Debug.Log("In MaxHP, value of MaxHP is " + maxHP);
+            PlayerController.PlayerUI.SetMaxHealth(maxHP);
+            if (HP + (maxHP - HP) > maxHP)
+            {
+                PlayerController.PlayerUI.SetHealth(maxHP);
+            }
+            else
+            {
+                HP += 10;
+            }
+        }
+        get { return maxHP; }
+    }
+
+    public float HP
+    {
+        set
+        {
+            hP = value;
+            if (ReadyToGo)
+            {
+                PlayerUI.SetHealth(hP);
+                if (hP <= 0)
+                {
+                    StartSpot = GameObject.Find("StartSpot").transform;
+                    Owner.TeleportTo(StartSpot.position, StartSpot.rotation);
+                }
+            }
+        }
+        get { return hP; }
+    }
+
+    public float XP
+    {
+        set
+        {
+            xP = value;
+            if (Networking.LocalPlayer == Owner)
+            {
+                if (!ReadyToGo)
+                    return;
+                Scoreboard = PlayerController.Scoreboard.GetComponent<Scoreboard>();
+                Scoreboard.SendCustomNetworkEvent(NetworkEventTarget.All, "UpdateBoard");
+                if (xP >= XPToNextLv && Level <= 20)
+                {
+                    Level++;
+                    EffectsContainer.SendCustomNetworkEvent(
+                        NetworkEventTarget.All,
+                        nameof(LevelUp)
+                    );
+                    XPToNextLv *= 1.2f;
+                    PlayerController.LevelUpUI.gameObject.SetActive(true);
+                    PlayerController.SetUIWAS("Fireball", Level.ToString());
+                    xP = 0;
+                }
+            }
+        }
+        get { return xP; }
+    }
+
+    public float RunSpeed
+    {
+        set
+        {
+            runSpeed = value;
+            if (Networking.LocalPlayer == Owner)
+            {
+                Owner.SetRunSpeed(RunSpeed);
+            }
+        }
+        get { return runSpeed; }
+    }
+
+    public void LevelUp() { }
+
+    public void TestingUI()
+    {
+        Debug.Log("In FireballPool");
+        Debug.Log("isFireballOn");
+        Debug.Log(isFireballOn);
+    }
+}
